e6c0566bc0aaff57157f61cebfc22d8963563742
ganu <gwbaik9717@icloud.com>
2025-01-30 18:07:04 +0900

    feat: 라우터의 navigate 메소드를 호출하면 history 스택에 새로운 엔트리가 추가되고, URL이 변경된다.

--- src/router.ts
+++ src/router.ts
@@ -2,27 +2,46 @@ type _Function = (...args: any) => any;
 
 export const createRouter = () => {
   const routes = new Map<string, _Function>();
-  let globalWindow: Window;
+  let isInitialized = false;
+  let userWindow: Window;
 
   const router = {
-    init: (window: Window) => {
-      globalWindow = window;
+    init: (window: Window = globalThis.window) => {
+      if (!window) {
+        throw new Error("Router requires a window object");
+      }
 
-      window.addEventListener("popstate", () => {});
+      userWindow = window;
+      userWindow.addEventListener("popstate", () => {});
+
+      isInitialized = true;
     },
 
     addRoute: (path: string, handler: _Function) => {
       routes.set(path, handler);
     },
 
-    push: (path: string) => {
+    navigate: (path: string) => {
+      if (!isInitialized) {
+        throw new Error("Router should be initialized first");
+      }
+
       const handler = routes.get(path);
 
       if (!handler) {
         throw new Error("Path is not registered");
       }
 
-      handler();
+      const newUrl = new URL(path, userWindow.location.origin).toString();
+      userWindow.history.pushState({}, "", newUrl);
+
+      try {
+        handler();
+      } catch (e: unknown) {
+        if (e instanceof Error) {
+          throw e;
+        }
+      }
     },
   };
 
--- tests/router.test.ts
+++ tests/router.test.ts
@@ -18,7 +18,7 @@ describe("unit test for configuring router", () => {
     document = window.document;
   });
 
-  test("라우터는 현재 URL을 기반으로 페이지 콘텐츠를 렌더링할 수 있다.", () => {
+  test("라우터의 navigate 메소드를 호출하면 등록한 핸들러가 실행된다.", () => {
     const router = createRouter();
     const app = document.getElementById("app");
 
@@ -35,7 +35,29 @@ describe("unit test for configuring router", () => {
     router.init(window as unknown as Window);
     router.addRoute("/", Component);
 
-    router.push("/");
+    router.navigate("/");
     expect(app.innerText).toBe(innerText);
+  });
+
+  test("라우터의 navigate 메소드를 호출하면 history 스택에 새로운 엔트리가 추가되고, URL이 변경된다.", () => {
+    const router = createRouter();
+    const app = document.getElementById("app");
+
+    if (!app) {
+      throw new Error("Root Element not found");
+    }
+
+    const innerText = "Test Page";
+
+    const Component = () => {
+      app.innerText = innerText;
+    };
+
+    router.init(window as unknown as Window);
+    router.addRoute("/test", Component);
+    router.navigate("/test");
+
+    expect(window.location.href).toBe("http://localhost/test");
+    expect(window.history.length).toBe(2);
   });
 });
