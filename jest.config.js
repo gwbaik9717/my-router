65d599937d66ebada82b8c2f6f2ecc8e8db5079b
ganu <gwbaik9717@icloud.com>
2025-01-31 22:31:01 +0900

    chore: 프로젝트 빌드 환경 설정 및 npm publish 설정

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,12 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   rootDir: path.resolve(__dirname),
-  moduleDirectories: ["node_modules", "src"],
+  moduleDirectories: ["node_modules"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testMatch: ["<rootDir>/tests/**/*.test.ts"],
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.test.json" }],
   },
 };
